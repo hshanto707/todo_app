468b3aaed4617dccb740c359186e971a89ab6070
Hasibul Hasan <hshanto707@gmail.com>
2023-01-22 17:38:55 +0600

    sign in & sign up page added

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
-def login():
-  return "<h1>Login</h1>"
+@auth.route('/sign-in')
+def sign_in():
+  return render_template("sign_in.html")
+
+@auth.route('/sign-up')
+def sign_up():
+  return render_template("sign_up.html")
 
 @auth.route('/logout')
 def logout():
   return "<h1>Logout</h1>"
-
-@auth.route('/sign-up')
-def sign_up():
-  return "<h1>Sign Up</h1>"
